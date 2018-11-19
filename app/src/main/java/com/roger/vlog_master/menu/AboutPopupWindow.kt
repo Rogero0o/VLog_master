2172b0e5756ab0a0e2ba6cad401d38908cd8d682
Roger Luo <roger.luo@ringcentral.com>
2018-11-19 09:46:26 +0800

    add feedback logic

--- app/src/main/java/com/roger/vlog_master/menu/AboutPopupWindow.kt
+++ app/src/main/java/com/roger/vlog_master/menu/AboutPopupWindow.kt
@@ -1,6 +1,11 @@
 package com.roger.vlog_master.menu
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.view.View
+import com.roger.vlog_master.R
+
 
 class AboutPopupWindow(c: Context, layoutRes: Int, w: Int, h: Int) : BasePopupWindow(c, layoutRes, w, h) {
 
@@ -8,6 +13,25 @@ class AboutPopupWindow(c: Context, layoutRes: Int, w: Int, h: Int) : BasePopupWi
     }
 
     override fun initEvent() {
+        contentView?.findViewById<View>(R.id.feedback)?.setOnClickListener {
+            sendEmail()
+            instance.dismiss()
+        }
+    }
 
+    private fun sendEmail() {
+        val data = Intent(Intent.ACTION_SENDTO)
+        data.data = Uri.parse("mailto:flyrogermail@gmail.com")
+        data.putExtra(Intent.EXTRA_SUBJECT, "VLogMaster Feedback")
+        val info =
+            "Device: " + android.os.Build.MANUFACTURER + "-" + android.os.Build.MODEL + "\n" + "Version: " + getVersionName()
+        data.putExtra(Intent.EXTRA_TEXT, info)
+        context.startActivity(data)
+    }
+
+    private fun getVersionName(): String {
+        val packageManager = context.packageManager
+        val packInfo = packageManager.getPackageInfo(context.packageName, 0)
+        return packInfo.versionName
     }
 }
